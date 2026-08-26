0abbc30ca99f930423ecc7876d0737b4dce17447
NIKHILxRAJ <raj.nikhilraj333.com>
2026-08-27 00:46:24 +0530

    Week 1 Day 3: ToKens revison

--- week1/day3/tokens.py
+++ week1/day3/tokens.py
@@ -1,41 +1,55 @@
 import os
+
 from pathlib import Path
+
 from dotenv import load_dotenv
+
 from groq import Groq
 
 load_dotenv()
-my_api_key=os.getenv("GROQ_API_KEY")
+
+my_api_key = os.getenv("GROQ_API_KEY")
 
 if not my_api_key:
     raise ValueError("API key kaha hai bhai")
 
-client=Groq(api_key=my_api_key)
+client = Groq(api_key=my_api_key)
 
-model="llama-3.3-70b-versatile"
-role="user"
-# // 3 prompts
+model = "openai/gpt-oss-120b"
+
+role = "user"
+
+# 3 prompts
+
 prompt1 = "Hi!"
+
 prompt2 = "Explain time travel in Detail but under 100 words"
+
 prompt3 = "Write a 1000 word essay on Machine learning"
 
-prompts=[prompt1,prompt2,prompt3]
+prompts = [prompt1, prompt2, prompt3]
+
 for prompt in prompts:
-    message={
-    "role": role,
-    "content": prompt
+
+    message = {
+        "role": role,
+        "content": prompt
     }
-    messages=[message]
-    response=client.chat.completions.create(model=model, messages=messages, max_tokens=5000)
-    usage=response.usage
-    print(f"Prompt: {prompt} -->your tokens: {usage.prompt_tokens} completion_tokens: {usage.completion_tokens} total tokens: {usage.total_tokens}  Finish Reason: {response.choices[0].finish_reason}")
 
-# prompt="hello my friend"
-# # message me role and content
-# message={
-#     "role": role,
-#     "content": prompt
-# }
+    messages = [message]
 
-# messages=[message]
+    response = client.chat.completions.create(
+        model=model,
+        messages=messages,
+        max_tokens=5000
+    )
 
-# response=client.chat.completions.create(model=model, messages=messages)
+    usage = response.usage
+
+    print(
+        f"Prompt: {prompt} --> "
+        f"Your tokens: {usage.prompt_tokens} "
+        f"Completion tokens: {usage.completion_tokens} "
+        f"Total tokens: {usage.total_tokens} "
+        f"Finish Reason: {response.choices[0].finish_reason}"
+    )
